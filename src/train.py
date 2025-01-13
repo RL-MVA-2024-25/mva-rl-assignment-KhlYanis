74505d442c94825ea7ee8cd61d39c98e2838c788
Yaniiiuss <yanis.kahil01@gmail.com>
2025-01-13 23:29:28 +0100

    Hoping that it will work

--- src/train.py
+++ src/train.py
@@ -14,36 +14,6 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 
-class DQN (torch.nn.Module):
-    def __init__(self, config):
-        super(DQN, self).__init__()
-        
-        # Setup the parameters of the problem
-        self.nb_actions = config['nb_actions']
-        self.nb_states = config['nb_states']
-        self.hidden_dim = config['hidden_dim']
-
-        # Setup the device
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-
-        # Initialize the DQN block
-        self.InitNNBlock()
-
-    def InitNNBlock(self):
-        # Initialize the DQN
-        self.dqnet = nn.Sequential(
-            nn.Linear(self.nb_states, self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, self.nb_actions)
-        ).to(self.device)
-
-    def forward(self, x):
-        return self.dqnet(x)
-    
 
 def greedy_action(network, state):
     device = "cuda" if next(network.parameters()).is_cuda else "cpu"
@@ -89,5 +59,5 @@ class ProjectAgent:
 
     def load(self):
         self.model = self.Init_DQN()
-        self.model.load_state_dict(torch.load(self.model_path, self.device))
+        self.model.load_state_dict(torch.load(self.model_path, map_location = self.device))
         self.model.eval()
