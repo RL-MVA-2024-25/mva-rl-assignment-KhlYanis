0f0129219cf3c9001fbcd3829c17c10d29fa9317
Yaniiiuss <yanis.kahil01@gmail.com>
2025-01-13 23:11:47 +0100

    Fix a small issue

--- src/train.py
+++ src/train.py
@@ -2,7 +2,6 @@ from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 import torch
 import torch.nn as nn
-from src.dqn import DQN
 
 
 env = TimeLimit(
@@ -15,6 +14,36 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 
+class DQN (torch.nn.Module):
+    def __init__(self, config):
+        super(DQN, self).__init__()
+        
+        # Setup the parameters of the problem
+        self.nb_actions = config['nb_actions']
+        self.nb_states = config['nb_states']
+        self.hidden_dim = config['hidden_dim']
+
+        # Setup the device
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # Initialize the DQN block
+        self.InitNNBlock()
+
+    def InitNNBlock(self):
+        # Initialize the DQN
+        self.dqnet = nn.Sequential(
+            nn.Linear(self.nb_states, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.nb_actions)
+        ).to(self.device)
+
+    def forward(self, x):
+        return self.dqnet(x)
+    
 
 def greedy_action(network, state):
     device = "cuda" if next(network.parameters()).is_cuda else "cpu"
@@ -23,7 +52,7 @@ def greedy_action(network, state):
         return torch.argmax(Q).item()
 
 class ProjectAgent:
-    
+
     def __init__(self, model_path = 'AgentDQN.pt'):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
