3fd80a5779ce333b186b5565a08e961e955e6948
Yaniiiuss <yanis.kahil01@gmail.com>
2025-01-14 10:03:56 +0100

    Another test

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 import torch
 import torch.nn as nn
+from src.dqn import DQN
 
 
 env = TimeLimit(
@@ -15,25 +16,24 @@ env = TimeLimit(
 # ENJOY!
 
 
-def greedy_action(network, state):
-    device = "cuda" if next(network.parameters()).is_cuda else "cpu"
-    with torch.no_grad():
-        Q = network(torch.Tensor(state).unsqueeze(0).to(device))
-        return torch.argmax(Q).item()
+class DQN (torch.nn.Module):
+    def __init__(self, config):
+        super(DQN, self).__init__()
 
-class ProjectAgent:
+        # Setup the parameters of the problem
+        self.nb_actions = config['nb_actions']
+        self.nb_states = config['nb_states']
+        self.hidden_dim = config['hidden_dim']
 
-    def __init__(self, model_path = 'AgentDQN.pt'):
+        # Setup the device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        self.nb_actions = 4
-        self.hidden_dim = 256
-        self.nb_states = 6
-        
-        self.model_path = model_path
+        # Initialize the DQN block
+        self.InitNNBlock()
 
-    def Init_DQN(self):
-        dqnet = nn.Sequential(
+    def InitNNBlock(self):
+        # Initialize the DQN
+        self.dqnet = nn.Sequential(
             nn.Linear(self.nb_states, self.hidden_dim),
             nn.ReLU(),
             nn.Linear(self.hidden_dim, self.hidden_dim),
@@ -47,7 +47,25 @@ class ProjectAgent:
             nn.Linear(self.hidden_dim, self.nb_actions)
         ).to(self.device)
 
-        return dqnet
+    def forward(self, x):
+        return self.dqnet(x)
+
+def greedy_action(network, state):
+    device = "cuda" if next(network.parameters()).is_cuda else "cpu"
+    with torch.no_grad():
+        Q = network(torch.Tensor(state).unsqueeze(0).to(device))
+        return torch.argmax(Q).item()
+
+class ProjectAgent:
+    
+    def __init__(self, model_path = 'AgentDQN.pt'):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.config = {'nb_actions' : 4,
+                        'hidden_dim' : 256,
+                        'nb_states' : 6}
+        
+        self.model_path = model_path
 
     def act(self, observation):
         with torch.no_grad():
@@ -58,6 +76,8 @@ class ProjectAgent:
         pass
 
     def load(self):
-        self.model = self.Init_DQN()
-        self.model.load_state_dict(torch.load(self.model_path))
+        self.model = DQN(self.config).to(self.device)
+        self.model.load_state_dict(
+            torch.load(self.model_path, map_location = torch.device("cpu"))
+        )
         self.model.eval()
